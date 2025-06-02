106de142025feb29cdbe1cad3efdcd773a73b0c0
web22des <web22designer@yandex.ru>
2025-06-02 23:31:55 +0300

    fix link 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 // config/astro/astro.config.mjs
 import { defineConfig } from "astro/config";
+import viteConfig from "./vite.config.js";
 
-// const isDev = process.env.NODE_ENV === "development";
-// const isPreview = process.argv.includes("preview");
-// const isGHPages = process.env.GITHUB_ACTIONS === "true"; // Автоопределение GitHub Actions
-
-// Автоматически определяем режим (dev/prod) и GitHub Pages
 const isDev = process.env.NODE_ENV === "development";
-const isGHPages = process.env.GITHUB_ACTIONS === "true"; // GitHub Actions
+const isPreview = process.argv.includes("preview");
+const isGHPages = process.env.GITHUB_ACTIONS === "true"; // Автоопределение GitHub Actions
 
 export default defineConfig({
     site: "https://web22des.github.io",
-    base: isDev ? "/" : "/proto-parsec/", // Локально "/", на GitHub "/proto-parsec/"
-    trailingSlash: "always", // Добавляет "/" в конец URL
+    base: isDev || isPreview || !isGHPages ? "/" : "/proto-parsec/",
+    trailingSlash: "always",
+    output: "static", // Явно указываем статический режим
+    vite: viteConfig,
 });
