5405e60a0c21bbd683abbc33fc7a889dd9af86a1
web22des <web22designer@yandex.ru>
2025-06-02 23:01:06 +0300

    fix links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // config/astro/astro.config.mjs
 import { defineConfig } from "astro/config";
 
-const isDev = process.env.NODE_ENV === "development";
-const isPreview = process.argv.includes("preview");
-const isGHPages = process.env.GITHUB_ACTIONS === "true"; // Автоопределение GitHub Actions
+// const isDev = process.env.NODE_ENV === "development";
+// const isPreview = process.argv.includes("preview");
+// const isGHPages = process.env.GITHUB_ACTIONS === "true"; // Автоопределение GitHub Actions
 
 export default defineConfig({
     site: "https://web22des.github.io",
-    base: isDev || isPreview || !isGHPages ? "/" : "/astro-test-5/",
-    trailingSlash: "always",
+    base: "/proto-parsec/",
+    trailingSlash: "ignore",
+    //base: isDev || isPreview || !isGHPages ? "/" : "/astro-test-5/",
+    //trailingSlash: "always",
     output: "static", // Явно указываем статический режим
 });
